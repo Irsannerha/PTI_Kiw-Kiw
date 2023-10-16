c151adfbc77cca715a0aac0c6bbdc595048d32b2
hariando7 <rianaja2200022@gmail.com>
2023-10-16 16:02:15 +0700

    update ando

---
+++ src/app/components/NavbarPemesanan.tsx
@@ -0,0 +1,12 @@
+import Image from 'next/image'
+
+// import { useDispatch } from "react-redux";
+// import { setSearch } from "../redux/slices/SearchSlice";
+
+export default function NavbarPemesanan() {
+    return (
+        <>
+            <p>Navbar Pemesanan</p>
+        </>
+    )
+}
---
+++ src/app/page/page.tsx
@@ -0,0 +1,10 @@
+import NavbarPemesanan from '../components/NavbarPemesanan'
+import Image from 'next/image'
+
+export default function Home() {
+    return (
+        <>
+            <NavbarPemesanan />
+        </>
+    )
+}
