0902b43da7502f41d9d2681e4d9078be9793d87d
hariando7 <rianaja2200022@gmail.com>
2023-10-16 15:29:47 +0700

    update ando

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -107,6 +107,7 @@ export default function Home() {
             Instantly deploy your Next.js site to a shareable URL with Vercel.
           </p>
           <p>andoooooooooooooooooooooooooooooooooo</p>
+          <p>asdasfsdgdfgdfg</p>
         </a>
       </div>
     </main>
